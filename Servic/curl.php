cf9bbfd92554d11605e30e877295093821abe8fc
Sahaj Totla <143136542+sahaj-totla@users.noreply.github.com>
2023-12-06 15:20:37 -0800

    ok

--- Servic/curl.php
+++ Servic/curl.php
@@ -2,47 +2,13 @@
 session_start();
 
 // Check if the admin is logged in, if not redirect to login page
-
-// The URL where the request is sent
-$pixel_url = "http://pixelquestgames-sahaj-totla.us-west-1.elasticbeanstalk.com/users.php"; // Replace with the correct URL to users.php
+if (!isset($_SESSION['admin_logged_in']) || !$_SESSION['admin_logged_in']) {
+    header('Location: /login.php');
+    exit();
+}
+// The URL where the request is sent // Replace with the correct URL to users.php
 $genai_url = "http://genaiwear.us-west-1.elasticbeanstalk.com/users.php";
-$servic_url = "http://ecommerce.us-west-1.elasticbeanstalk.com/users.php";
 // Initialize cURL session
-$ch = curl_init($pixel_url);
-
-// Set cURL options
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_HEADER, false);
-
-// Execute cURL session and get the response data
-$response = curl_exec($ch);
-
-// Check for cURL errors
-if(curl_errno($ch)){
-    throw new Exception(curl_error($ch));
-}
-
-// Close cURL session
-curl_close($ch);
-
-// Decode JSON response
-$users = json_decode($response, true);
-
-// Check if the response is an array of users
-if (is_array($users)) {
-    echo "<h1>PixelQuest Games User List</h1>";
-    foreach ($users as $user) {
-        echo "User: " . htmlspecialchars($user['first_name']) . " " . htmlspecialchars($user['last_name']) . 
-             "<br>Email: " . htmlspecialchars($user['email']) . 
-             "<br>Home Phone: " . htmlspecialchars($user['home_phone']) . 
-             "<br>Mobile Phone: " . htmlspecialchars($user['cell_phone']) . 
-             "<br>Home Address: " . htmlspecialchars($user['home_address']) . "<br><br>";
-    }
-} else {
-    echo "No users found or the API endpoint did not return valid JSON.";
-}
-
-// GENAI curl
 $ch = curl_init($genai_url);
 
 // Set cURL options
@@ -65,42 +31,7 @@ $users = json_decode($response, true);
 
 // Check if the response is an array of users
 if (is_array($users)) {
-    echo "<h1>GenAI Wear User List</h1>";
-    foreach ($users as $user) {
-        echo "User: " . htmlspecialchars($user['first_name']) . " " . htmlspecialchars($user['last_name']) . 
-             "<br>Email: " . htmlspecialchars($user['email']) . 
-             "<br>Home Phone: " . htmlspecialchars($user['home_phone']) . 
-             "<br>Mobile Phone: " . htmlspecialchars($user['cell_phone']) . 
-             "<br>Home Address: " . htmlspecialchars($user['home_address']) . "<br><br>";
-    }
-} else {
-    echo "No users found or the API endpoint did not return valid JSON.";
-}
-
-// SERVIC curl
-$ch = curl_init($servic_url);
-
-// Set cURL options
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_HEADER, false);
-
-// Execute cURL session and get the response data
-$response = curl_exec($ch);
-
-// Check for cURL errors
-if(curl_errno($ch)){
-    throw new Exception(curl_error($ch));
-}
-
-// Close cURL session
-curl_close($ch);
-
-// Decode JSON response
-$users = json_decode($response, true);
-
-// Check if the response is an array of users
-if (is_array($users)) {
-    echo "<h1>Servic User List</h1>";
+    echo "<h1>GenAIwear User List</h1>";
     foreach ($users as $user) {
         echo "User: " . htmlspecialchars($user['first_name']) . " " . htmlspecialchars($user['last_name']) . 
              "<br>Email: " . htmlspecialchars($user['email']) . 
