e4a1283ee437ff88191108547cf407ead9a8c25c
Sahaj Totla <143136542+sahaj-totla@users.noreply.github.com>
2023-12-05 20:54:58 -0800

    y

--- index.php
+++ index.php
@@ -13,7 +13,22 @@ $is_admin_logged_in = isset($_SESSION['admin_logged_in']) && $_SESSION['admin_lo
         body { font-family: Arial, sans-serif; margin: 0; padding: 0; background-color: #f4f4f4; }
         .header { background-color: #333; color: white; padding: 10px 0; text-align: center; }
         .nav { padding: 15px; background: #444; }
-        .nav a { color: white; margin: 0 15px; text-decoration: none; }
+        .nav button {background-color: #ffcd29;
+    border: none;
+    color: black;
+    font-family: "Arial Rounded MT Bold", sans-serif;
+    font-size: 15px;
+    padding: 10px 20px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    cursor: pointer;
+    border-radius: 16px;
+    width: 10%;
+    min-width: 100px;
+    height: 40px;
+    transition: background-color 0.3s;
+    display: inline-block;}
         .main { padding: 20px; }
         .card { background: white; padding: 20px; margin-bottom: 20px; border-radius: 5px; box-shadow: 0 2px 4px rgba(0,0,0,0.1); }
         .footer { background-color: #333; color: white; text-align: center; padding: 10px 0; margin-top: 30px; }
@@ -31,7 +46,7 @@ $is_admin_logged_in = isset($_SESSION['admin_logged_in']) && $_SESSION['admin_lo
         <!-- Display Sign In Button for guests -->
         <button class="button" onclick="location.href='login.php';">Log in</button>
     <?php endif; ?>
-    <button class="button" onclick="location.href='register.html';">register</button>
+        <button class="button" onclick="location.href='register.html';">register</button>
     </div>
     <div class="main">
         <div class="card">
